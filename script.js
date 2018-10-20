a473d071691f1577eea55fe42f2495762f8cf59d
Aaron Smith <aaronsmith.xci@gmail.com>
2018-10-20 09:52:07 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+(function() {
+
+  const todos = [];
+  todos.push('Mow the lawn');
+  todos.push('Buy milk');
+  console.log(todos);
+
+  const todoUl = document.querySelector();
+
+  function render() {
+    // Clear existing to-do list
+
+
+    // Render to-do list items
+  }
+
+})();
